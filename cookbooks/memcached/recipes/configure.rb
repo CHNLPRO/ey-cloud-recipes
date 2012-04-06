103e71e374677f3943eaf99b1137c6779decae17
Adam Bozanich <adam.boz@gmail.com>
2012-04-06 16:17:57 -0700

    fix memcached for solo

--- cookbooks/memcached/recipes/configure.rb
+++ cookbooks/memcached/recipes/configure.rb
@@ -3,13 +3,25 @@
 # Recipe:: configure
 #
 
-template "/data/chnl/shared/config/memcached.yml" do
-  source "memcached.yml.erb"
-  owner node[:owner_name]
-  group node[:owner_name]
-  mode 0644
-  variables({
-      :app_name     => 'chnl',
-      :server_names => Array(node[:members]).sort
-  })
+if node[:instance_role] == 'solo'
+  template "/data/chnl/shared/config/memcached.yml" do
+    source "memcached.yml.erb"
+    owner node[:owner_name]
+    group node[:owner_name]
+    mode 0644
+    variables({
+        :app_name     => 'chnl',
+    })
+  end
+else
+  template "/data/chnl/shared/config/memcached.yml" do
+    source "memcached.yml.erb"
+    owner node[:owner_name]
+    group node[:owner_name]
+    mode 0644
+    variables({
+        :app_name     => 'chnl',
+        :server_names => Array(node[:members]).sort
+    })
+  end
 end
