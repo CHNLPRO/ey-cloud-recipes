7dc41d5fdf358dc65c7d08492e227708c89ccac4
Tushar Ranka <tusharranka@gmail.com>
2013-09-22 10:12:12 -0700

    Revert "change over attribute to node"
    This reverts commit 0887a292e0d4a2dacb15637143223bae883f2194.

--- cookbooks/main/attributes/recipe.rb
+++ cookbooks/main/attributes/recipe.rb
@@ -1,4 +1,4 @@
 recipes('main')
-owner_name(node[:users].first[:username])
-owner_pass(node[:users].first[:password])
+owner_name(@attribute[:users].first[:username])
+owner_pass(@attribute[:users].first[:password])
 newrelic_license_key("32f303544cbdf79c790bcd9efbbc5369e5a27d43")
